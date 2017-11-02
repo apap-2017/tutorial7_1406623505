8601fed92fec35db65ba7006c063c1186359fb7a
Hawari Hilman <hawarihilman@gmail.com>
2017-11-02 19:57:34 +0800

    latihan 1

--- Tutorial7Producer/src/main/java/com/example/rest/StudentRestController.java
+++ Tutorial7Producer/src/main/java/com/example/rest/StudentRestController.java
@@ -1,5 +1,7 @@
 package com.example.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,16 @@ public class StudentRestController
 	@Autowired
 	StudentService studentService;
 	@RequestMapping("/student/view/{npm}")
-	
-	
 	public StudentModel view (@PathVariable(value = "npm") String npm) 
 	{
 		StudentModel student = studentService.selectStudent (npm);
+		return student;
+	}
+	
+	@RequestMapping("/student/viewall")
+	public List <StudentModel> viewall () 
+	{
+		List <StudentModel> student = studentService.selectAllStudents();
 		return student;
 	}
 }
