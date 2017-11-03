89d6ae9fdb4c1f58f1bb57e10da0409a6ee06b0a
Hawari Hilman <hawarihilman@gmail.com>
2017-11-03 12:38:28 +0800

    latihan 3 + writeup

---
+++ Tutorial07Consumer/src/main/java/com/example/service/StudentServiceRest.java
@@ -0,0 +1,57 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.StudentDAO;
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+import groovy.util.logging.Slf4j;
+
+@Slf4j
+@Service
+@Primary
+public class StudentServiceRest implements StudentService {
+
+	@Autowired
+	private StudentDAO studentDAO;
+	@Override
+	public StudentModel selectStudent(String npm) {
+		return studentDAO.selectStudent (npm);
+	}
+
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		// TODO Auto-generated method stub
+		return studentDAO.selectAllStudents();
+	}
+
+	@Override
+	public void addStudent(StudentModel student) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteStudent(String npm) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateStudent(StudentModel student) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public CourseModel selectAllStudentsCourse(String idCourse) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
